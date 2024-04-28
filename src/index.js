9414b6f8d1e7b52118b12c3211e03f66678aaff0
jafferm <mohiuddin293@gmail.com>
2024-04-28 02:14:01 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import{
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -12,9 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>
 );
 
