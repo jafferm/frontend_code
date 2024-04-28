e3bf2cf23acbf95f2efdbc4f42b71553c9f57ce0
jafferm <mohiuddin293@gmail.com>
2024-04-28 02:10:12 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import{
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
